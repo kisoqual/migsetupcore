dca8d8a556e6327e3284b30a8ea76e16f4c4a5ae
kisoqual <64285271+kisoqual@users.noreply.github.com>
2026-05-15 16:35:57 +0300

    Create migsetupcore.ps1

---
+++ migsetupcore.ps1
@@ -0,0 +1,40 @@
+$migVarName;
+$choice = Read-Host "Установить EntityFramework перед миграцией? (Y/N)";
+    
+function Menu {
+    Write-Output "`n----Мастер миграции Code First в EF Core----";
+    Write-Output "1. Добавление миграции`n2. Выйти";
+    $choice = Read-Host;
+    if ($choice -eq "1") {
+        AddMigration($migVarName);
+        Pause;
+        exit;
+    }
+    if ($choice -eq "2") {
+        Pause;
+        exit;
+    }
+    else {
+        Clear-Host;
+        Write-Output "Неверный ввод!";
+        Menu;
+    }
+}  
+
+function AddMigration {
+    param ($migInnerVarName)
+    $migInnerVarName = Read-Host "Введите имя переменной для миграции: ";
+    dotnet ef migrations add $migInnerVarName;
+    dotnet ef database update;
+}
+
+if ($choice -eq "y") {
+    dotnet tool install --global dotnet-ef;
+    $choice = Read-Host "Установить EntityFramework.Design перед миграцией? (Y/N)";
+    if ($choice -eq "y") {
+        dotnet add package Microsoft.EntityFrameworkCore.Design;
+        Menu;
+    }
+    else { Menu; }
+}
+else { Menu; }
